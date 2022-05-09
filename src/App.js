b396f1c50f0cf4ee96798f7183f4324e37f28ae2
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-10 00:14:56 +0600

    all route setted with pages skeleton

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./Pages/About/About";
+import Appoinment from "./Pages/Appoinment/Appoinment";
+import Contact from "./Pages/Contact/Contact";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Review from "./Pages/Review/Review";
 import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
@@ -9,6 +14,12 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/appoinment" element={<Appoinment />}></Route>
+        <Route path="/review" element={<Review />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </div>
   );
