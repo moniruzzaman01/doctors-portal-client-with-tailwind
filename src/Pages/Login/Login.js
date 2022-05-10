32e6d76cddd8a255cce2b951a65a61d2fb4bc2f7
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-11 00:10:39 +0600

    login component added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,30 @@
 import React from "react";
+import InputTag from "../Shared/InputTag";
 
 const Login = () => {
   return (
-    <div>
-      <h2>this is login page.</h2>
+    <div className=" lg:max-w-lg mx-auto my-20">
+      <h2 className="text-5xl text-center mb-10">Login</h2>
+      <form>
+        <label htmlFor="">Email</label>
+        <br />
+        <InputTag></InputTag>
+        <br />
+        <label htmlFor="">Password</label>
+        <br />
+        <InputTag></InputTag>
+        <br />
+        <p>Forgot password?</p>
+        <button className="btn btn-accent w-full text-white mt-2">login</button>
+        <p className="text-center mt-2">
+          New to Doctor's Portal?{" "}
+          <span className="text-primary">Create new account</span>
+        </p>
+      </form>
+      <div class="flex flex-col w-full border-opacity-50">
+        <div class="divider my-14">OR</div>
+      </div>
+      <button class="btn btn-outline w-full">continue with google</button>
     </div>
   );
 };
